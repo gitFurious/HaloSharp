1cb2eab0515a8709212f3aa1f81a41f26f81dd10
gitFurious <github@damonpollard.com>
2017-03-12 12:20:01 +0800

    Added example console application.

---
+++ Examples/HaloSharp.Console/Infrastructure/HaloSharpModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using HaloSharp.Model;
+using System;
+using System.Configuration;
+
+namespace HaloSharp.Console.Infrastructure
+{
+    internal class HaloSharpModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var subscriptionKey = ConfigurationManager.AppSettings["SubscriptionKey"];
+
+            var product = new Product
+            {
+                SubscriptionKey = subscriptionKey,
+                RateLimit = new RateLimit
+                {
+                    RequestCount = 200,
+                    TimeSpan = new TimeSpan(0, 0, 0, 10),
+                    Timeout = new TimeSpan(0, 0, 0, 10)
+                }
+            };
+
+            var cacheSettings = new CacheSettings
+            {
+                CacheDuration = new TimeSpan(0, 1, 0, 0)
+            };
+
+            var haloClient = new HaloClient(product, cacheSettings);
+            var haloSession = haloClient.StartSession();
+
+            builder.RegisterInstance(haloSession)
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
