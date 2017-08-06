38e57463bbd008d610b1377ea3f1b0e755f3ef69
Connor Kennelly <knelly26@gmail.com>
2017-08-05 19:46:22 -0700

    Add Player Appearance Model (Halo5.Profile endpoint)

---
+++ Source/HaloSharp/Model/Halo5/Profile/PlayerAppearance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Newtonsoft.Json;
+using HaloSharp.Model.Common;
+
+namespace HaloSharp.Model.Halo5.Profile
+{
+    [Serializable]
+    public class PlayerAppearance : IEquatable<PlayerAppearance>
+    {
+        [JsonProperty(PropertyName = "gamertag")]
+        public string Gamertag { get; set; }
+
+        [JsonProperty(PropertyName = "LastModifiedUtc")]
+        public ISO8601 LastModifiedUtc { get; set; }
+
+        [JsonProperty(PropertyName = "FirstModifiedUtc")]
+        public ISO8601 FirstModifiedUtc { get; set; }
+
+        [JsonProperty(PropertyName = "serviceTag")]
+        public string ServiceTag { get; set; }
+
+        [JsonProperty(PropertyName = "Company")]
+        public Company company { get; set; }
+
+        public bool Equals(PlayerAppearance other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return company.Equals(other.company)
+                && Gamertag == other.Gamertag
+                && LastModifiedUtc == other.LastModifiedUtc
+                && FirstModifiedUtc == other.FirstModifiedUtc
+                && ServiceTag == other.ServiceTag;
+        }
+    }
+
+    [Serializable]
+    public class Company : IEquatable<Company>
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+
+        public bool Equals(Company other)
+        {
+
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Id == other.Id
+                && Name == other.Name;
+        }
+    }
+
+
+
+
+}
