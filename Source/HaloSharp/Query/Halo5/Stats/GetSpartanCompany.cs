30ee9c3d1cb0eeb419fa94c4289c2b4d2a5dcdfb
Connor Kennelly <knelly26@gmail.com>
2017-09-17 09:29:55 -0700

    Add GetSpartanCompany query class. (Halo5.Query.Stats)

---
+++ Source/HaloSharp/Query/Halo5/Stats/GetSpartanCompany.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HaloSharp.Exception;
+using HaloSharp.Model;
+using HaloSharp.Model.Halo5.Stats;
+using HaloSharp.Validation.Common;
+
+namespace HaloSharp.Query.Halo5.Stats
+{
+    
+    class GetSpartanCompany : Query<SpartanCompany>
+    {
+        protected virtual string Path => $"stats/h5/companies/{_companyId}";
+
+        public override string Uri => HaloUriBuilder.Build(Path);
+
+        private readonly Guid _companyId;
+
+        public GetSpartanCompany(Guid companyId)
+        {
+            _companyId = companyId;
+        }
+
+        protected override void Validate()
+        {
+            var validationResult = new ValidationResult();
+
+            if (!_companyId.IsValid())
+            {
+                validationResult.Messages.Add("GetSpartanyCompany query requires a valid company Id to be set.");
+            }
+
+            if (!validationResult.Success)
+            {
+                throw new ValidationException(validationResult.Messages);
+            }
+        }
+    }
+}
